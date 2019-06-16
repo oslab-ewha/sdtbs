abcc59e131dcc71f537d7559ee7d25b495e30d7a
KyungWoon Cho <cezanne@codemayo.com>
2019-06-16 12:34:21 +0900

    support multiple native benchmarks
    Separate cuda streams should be used to run multiple benchmarks
    simultaneously.

--- src/sdtbs/tbs_native.cu
+++ src/sdtbs/tbs_native.cu
@@ -1,29 +1,39 @@
 #include "sdtbs_cu.h"
 
+#define SIZE_ARGS	(sizeof(void *) * MAX_ARGS)
+
+static cudaStream_t	strms[MAX_BENCHES];
+
 extern "C" BOOL
 run_native_tbs(void)
 {
 	benchrun_t	*brun;
-	cudaStream_t	strm;
+	void 	**d_args_brun;
 	int	i;
 
-	brun = benchruns;
-	cudaStreamCreate(&strm);
-	for (i = 0; i < n_benches; i++, brun++) {
-		void 	**d_args;
+	cudaMalloc(&d_args_brun, SIZE_ARGS * n_benches);
+	for (i = 0, brun = benchruns; i < n_benches; i++, brun++) {
+		cudaStreamCreate(strms + i);
+		cudaMemcpy((char *)d_args_brun + SIZE_ARGS * i, brun->args, SIZE_ARGS, cudaMemcpyHostToDevice);
+	}
+
+	for (i = 0, brun = benchruns; i < n_benches; i++, brun++) {
 		int	ret;
 
-		cudaMalloc(&d_args, sizeof(void *) * MAX_ARGS);
-		cudaMemcpy(d_args, brun->args, sizeof(void *) * MAX_ARGS, cudaMemcpyHostToDevice);
-		ret = brun->info->bench_func(strm, brun->n_grid_width, brun->n_grid_height, brun->n_tb_width, brun->n_tb_height, d_args);
-		if (ret < 0) {
+		ret = brun->info->bench_func(strms[i], brun->n_grid_width, brun->n_grid_height, brun->n_tb_width,
+					     brun->n_tb_height, (void **)((char *)d_args_brun + SIZE_ARGS * i));
+		if (ret < 0)
 			return FALSE;
-		}
-		cudaStreamSynchronize(strm);
-		cudaMemcpy(brun->args, d_args, sizeof(void *) * MAX_ARGS, cudaMemcpyDeviceToHost);
-		cudaFree(d_args);
+	}
+
+	for (i = 0; i < n_benches; i++)
+		cudaStreamSynchronize(strms[i]);
+
+	for (i = 0, brun = benchruns; i < n_benches; i++, brun++) {
+		cudaMemcpy(brun->args, d_args_brun + SIZE_ARGS * i, SIZE_ARGS, cudaMemcpyDeviceToHost);
 		brun->res = (int)(long long)brun->args[0];
 	}
+	cudaFree(d_args_brun);
 
 	return TRUE;
 }
