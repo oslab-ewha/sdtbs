d57dedb7544a84c66c0c0924ad9867e0c7fb584b
KyungWoon Cho <cezanne@codemayo.com>
2019-09-11 10:37:45 +0900

    add several funtionalities for run.sh
    - add TB increasing mode, benchmark direct mode
    - optionaly show headers of run.sh output

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ Usage: run.sh [options] <benchmark code:bench args only>...
  -p <policies>: hw,hwR,rr,rrS,rrf,rrfS,fca,rrm:64,rrmS:64
  -n <tbs per SM>, default: 1,2,3,4
  -t <threads per TB>, default: 32,64,128,256,512,1024
+ -T <#TB of start>,<#TB of end>: TB incresing mode
+ -x: benchmark direct mode(benchmark TB, threads should be specified) 
+ -H: display header
 EOF
 }
 
@@ -19,7 +22,8 @@ n_sms=1
 policies="hw,hwR,rrS,rrD,rr"
 n_tbs_per_sm="1,2,3,4,5,6,7,8"
 n_ths_per_tb="32,64,128,256,512,1024"
-while getopts "c:s:p:n:t:" arg
+tb_range=
+while getopts "c:s:p:n:t:T:xH" arg
 do
     case $arg in
 	c)
@@ -36,6 +40,15 @@ do
 	    ;;
 	t)
 	    n_ths_per_tb=$OPTARG
+	    ;;
+	T)
+	    tb_range=$OPTARG
+	    ;;
+	x)
+	    benchdirect_mode=true
+	    ;;
+	H)
+	    display_header=true
 	    ;;
 	*)
 	    usage
@@ -75,7 +88,7 @@ function run_sdtbs() {
     echo
 }
 
-function show_banner() {
+function show_header() {
     echo -n "#"
     for p in $(echo $policies | tr "," "\n")
     do
@@ -85,23 +98,42 @@ function show_banner() {
 }
 
 function run_sdtbs_tbs_threads() {
-    for m in $(echo $n_tbs_per_sm | tr "," "\n")
+    for ths in $(echo $n_ths_per_tb | tr "," "\n")
     do
-	tbs=$(($m * $n_sms))
-	for ths in $(echo $n_ths_per_tb | tr "," "\n")
+	cmd_args=
+	for arg in $*
 	do
-	    cmd_args=
-	    for arg in $*
-	    do
-		bench=`echo $arg | cut -d':' -f1`
-		bencharg=`echo $arg | cut -d':' -f2`
-		cmd_args="$cmd_args $bench:$tbs,1,$ths,1,$bencharg"
-	    done
-	    run_sdtbs $cmd_args
+	    bench=`echo $arg | cut -d':' -f1`
+	    bencharg=`echo $arg | cut -d':' -f2`
+	    cmd_args="$cmd_args $bench:$tbs,1,$ths,1,$bencharg"
 	done
+	run_sdtbs $cmd_args
     done
 }
 
-show_banner
+function run_sdtbs_tb_per_sm() {
+    for m in $(echo $n_tbs_per_sm | tr "," "\n")
+    do
+	tbs=$(($m * $n_sms))
+	run_sdtbs_tbs_threads $*
+    done
+}
 
-run_sdtbs_tbs_threads $*
+function run_sdtbs_tbs_range() {
+    for tbs in $(seq $(echo $tb_range | tr "," " "))
+    do
+	run_sdtbs_tbs_threads $*
+    done
+}
+
+if [[ -n $display_header ]]; then
+    show_header
+fi
+
+if [[ -n $benchdirect_mode ]]; then
+    run_sdtbs $*
+elif [[ -n $tb_range ]]; then
+    run_sdtbs_tbs_range $*
+else
+    run_sdtbs_tb_per_sm $*
+fi
